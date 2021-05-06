f9681706a5201379b2a8dbc32a8d1898590fdafd
DianaEspitia <Diana.Andrea.Espitia.Torres@ibm.com>
2021-05-06 17:20:39 -0500

    Un Nuevo Commit

--- src/app/services/query.service.ts
+++ src/app/services/query.service.ts
@@ -10,9 +10,9 @@ export class QueryService {
   selectclients: Clientmodel;
   client: Clientmodel[];
 
-  readonly URL_API_CREATE_TRANSACTION = 'http://apicrudformongo-workshopdemo.openshift311-ea9753cca330b7f05a99ad5b2c8b5da1-0001.us-east.containers.appdomain.cloud/api/customers';
-  readonly URL_API_GET_TRANSACTIONS = 'http://apicrudformongo-workshopdemo.openshift311-ea9753cca330b7f05a99ad5b2c8b5da1-0001.us-east.containers.appdomain.cloud/api/customers';
-  readonly URL_API_DELETE = 'http://apicrudformongo-workshopdemo.openshift311-ea9753cca330b7f05a99ad5b2c8b5da1-0001.us-east.containers.appdomain.cloud/api/customers/';
+  readonly URL_API_CREATE_TRANSACTION = 'http://team-2021-backend-crud-mongo-angularwebcrudmongo.openshift-demo-ea9753cca330b7f05a99ad5b2c8b5da1-0000.us-east.containers.appdomain.cloud/api/customers/';
+  readonly URL_API_GET_TRANSACTIONS = 'http://team-2021-backend-crud-mongo-angularwebcrudmongo.openshift-demo-ea9753cca330b7f05a99ad5b2c8b5da1-0000.us-east.containers.appdomain.cloud/api/customers/';
+  readonly URL_API_DELETE = 'http://team-2021-backend-crud-mongo-angularwebcrudmongo.openshift-demo-ea9753cca330b7f05a99ad5b2c8b5da1-0000.us-east.containers.appdomain.cloud/api/customers/';
 
   constructor(public httpClient: HttpClient)
   {
